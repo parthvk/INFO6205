3120b08ad0e37c28bb532d0eb0f5e8e2cc4ac85f
Parth Vasant Kataria <kataria.p@northeastern.edu>
2022-07-24 20:37:11 -0400

    Assignment 5

--- src/main/java/edu/neu/coe/info6205/sort/par/Main.java
+++ src/main/java/edu/neu/coe/info6205/sort/par/Main.java
@@ -20,15 +20,21 @@ public class Main {
         processArgs(args);
         System.out.println("Degree of parallelism: " + ForkJoinPool.getCommonPoolParallelism());
         Random random = new Random();
-        int[] array = new int[2000000];
+        int[] array = new int[3000000];
+        System.out.println("The length of array is "+array.length);
         ArrayList<Long> timeList = new ArrayList<>();
-        for (int j = 50; j < 100; j++) {
-            ParSort.cutoff = 10000 * (j + 1);
+
+        int thread = (int)Math.pow(2,5);
+        ParSort.customPool = new ForkJoinPool(thread);
+        System.out.println("Number of threads "+thread);
+
+        for (int j = 1; j < 36; j++) {
+            ParSort.cutoff = array.length/200*(j+1);
             // for (int i = 0; i < array.length; i++) array[i] = random.nextInt(10000000);
             long time;
             long startTime = System.currentTimeMillis();
             for (int t = 0; t < 10; t++) {
-                for (int i = 0; i < array.length; i++) array[i] = random.nextInt(10000000);
+                for (int i = 0; i < array.length; i++) array[i] = random.nextInt(30000000);
                 ParSort.sort(array, 0, array.length);
             }
             long endTime = System.currentTimeMillis();
